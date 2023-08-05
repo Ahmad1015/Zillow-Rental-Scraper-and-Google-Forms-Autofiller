29e360c2a52d8a77212170da313e4e42eefac744
Ahmad <mahmedraza90@gmail.com>
2023-08-05 21:38:52 +0500

    writing initial method names

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+def main():
+    pass
+
+
+def scrapping_data():
+    pass
+
+
+
+
+def autofilling_google_forms():
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
